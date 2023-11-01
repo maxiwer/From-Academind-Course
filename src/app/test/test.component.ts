734f68770f67897b841b260549ee673940472b71
Max Fisher <fma1cr@gmail.com>
2023-11-01 21:27:40 +0600

    add component to test selectors

---
+++ src/app/test/test.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: '[app-not-server]',
+  templateUrl: './test.component.html',
+  styleUrls: ['./test.component.css']
+})
+export class TestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
