da383763b2cfe1161888fbe5cadcb2e44fb35d24
Murat Faiz <murat.faiz@samgau.com>
2023-12-05 13:19:03 +0600

    check input&output of components

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,14 +1,18 @@
-import {Component, inject, ViewEncapsulation} from '@angular/core';
+import {ChangeDetectionStrategy, Component, inject, Input, OnChanges, SimpleChanges} from '@angular/core';
 import {MockService} from "../services/mock.service";
-import {ActivatedRoute, Router} from "@angular/router";
-import {tap} from "rxjs/operators";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-server',
   templateUrl: 'server.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ServerComponent {
+export class ServerComponent implements OnChanges {
+  @Input() loremInput: { order: number, label: string } = { label: '', order: 0 };
   private router = inject(Router);
   mockService = inject(MockService);
-  inputLorem = this.router.events.pipe();
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(`~LOREMINPUT`, changes['loremInput'].currentValue);
+  }
 }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,15 +1,5 @@
-import {
-  AfterContentInit,
-  AfterViewChecked, AfterViewInit,
-  Component, DoCheck,
-  ElementRef, HostBinding, inject,
-  Input,
-  OnChanges,
-  OnInit,
-  SimpleChanges,
-  ViewChildren
-} from '@angular/core';
-import {Observable, of} from "rxjs";
+import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
+import {of} from "rxjs";
 import {delay} from "rxjs/operators";
 import {MockService} from "../services/mock.service";
 
@@ -17,11 +7,18 @@ import {MockService} from "../services/mock.service";
   selector: 'app-servers',
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ServersComponent {
   mockService = inject(MockService);
 
   isContentVisible$ = of(true).pipe(delay(5000))
   items$: any = this.mockService.simulateHttp([ 1, 2, 3, 4, 5 ], 1500);
+  lorem =  { label: '', order: 0 };
+  order = this.lorem.order;
+  secondExample: any;
 
+  loremMethod() {
+    this.lorem = {...this.lorem, order: ++this.order };
+  }
 }
